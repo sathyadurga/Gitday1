bb70bf4e2392c8c09017d60bfcfa639b1e62dd0f
sathyadurga <sathyadurga04@gmail.com>
2022-01-09 22:35:31 +0530

    task completed

---
+++ src/org/tcs/GitDay1.java
@@ -0,0 +1,15 @@
+package org.tcs;
+
+public class GitDay1 {
+	
+	public void Addition() {
+        System.out.println("Hellooooo");
+	}
+	public static void main(String[] args) {
+		GitDay1 git = new GitDay1();
+		git.Addition();
+		
+	}
+	
+
+}
